89eeb6a26d5d1d15f4fd5837b3578d1808fc41a7
axelrammon <axelrammon48@gmail.com>
2024-03-19 23:43:37 -0300

    ajustando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export function Header() {
-    return (
-        <strong className={styles.header}>Ignite Feed</strong>
-    );
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
 }
