430deb83b3be05a5bff9422e91c67d5b68fcb37c
axelrammon <axelrammon48@gmail.com>
2024-03-21 15:19:54 -0300

    finalizando estilização da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,15 +7,22 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover} 
-        src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D" />
+        src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D"
+      />
 
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://avatars.githubusercontent.com/u/18445997?v=4" 
+        />
+
         <strong>Axel Rammon</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
