174332bc7629b140d40bf038e84b0eedd2fd8c9d
axelrammon <axelrammon48@gmail.com>
2024-03-20 23:01:29 -0300

    melhorando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,9 @@ import styles from './Sidebar.module.css';
 export function Sidebar() {
   return(
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D" />
+      <img
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D" />
 
       <div className={styles.profile}>
         <strong>Axel Rammon</strong>
