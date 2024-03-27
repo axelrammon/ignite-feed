33db931f505d988aa7caa65ce8e792f4a71b272b
axelrammon <axelrammon48@gmail.com>
2024-03-26 22:12:08 -0300

    adicionando keys aos componentes que tem array para percorrer

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,8 @@ function App() {
         <main>
           {posts.map(post => {
             return (
-              <Post 
+              <Post
+                key={post.id} 
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}  
