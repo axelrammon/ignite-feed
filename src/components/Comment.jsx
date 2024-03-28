d11acdd902dae933be9de988a754d9e2a4855a7b
axelrammon <axelrammon48@gmail.com>
2024-03-27 21:52:30 -0300

    deixando funç

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,22 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     // console.log("deletar");
 
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return(
@@ -31,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
